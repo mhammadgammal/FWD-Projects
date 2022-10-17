89623836167ad6d87a330bae81b52ad25057e4ee
Mohamed Gammal <73348912+mhammadgammal@users.noreply.github.com>
2022-10-17 22:59:07 +0200

    removing redundant files

--- app/src/main/java/com/example/asteroidradar/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.example.asteroidradar
-
-object Constants {
-    const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
-    const val DEFAULT_END_DATE_DAYS = 7
-}
--- app/src/main/java/com/example/asteroidradar/repository/database/PictureOfDay.kt
+++
@@ -1,6 +0,0 @@
-package com.example.asteroidradar.repository.database
-
-
-data class PictureOfDay(
-    val media_type: String = "", val title: String, val url: String
-)
