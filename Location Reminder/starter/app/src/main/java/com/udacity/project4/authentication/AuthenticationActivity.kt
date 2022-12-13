4e49b2ff3c0696687e0975bb48eb7520f61fa9bc
Mohamed Gammal <73348912+mhammadgammal@users.noreply.github.com>
2022-12-13 02:08:25 +0200

    Adding Authentication Activity
    Adding Firebase Authentication feature

--- Location Reminder/starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ Location Reminder/starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,15 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -10,15 +17,41 @@ import com.udacity.project4.R
  */
 class AuthenticationActivity : AppCompatActivity() {
 
+    companion object {
+        private const val SIGN_IN_RESULT_CODE = 1001
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
-//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val loginButton = findViewById<Button>(R.id.loginBTN)
 
-//          TODO: If the user was authenticated, send him to RemindersActivity
+        if (currentUser != null) {
+            val intent = Intent(this, RemindersActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
-//          TODO: a bonus is to customize the sign in flow to look nice using :
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == SIGN_IN_RESULT_CODE) {
+            IdpResponse.fromResultIntent(data)
+        }
+    }
 
+    private fun signIn(){
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            SIGN_IN_RESULT_CODE
+        )
     }
 }
