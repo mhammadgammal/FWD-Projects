c332d4d9d8fb8630a8761b3b0aeb3d8953956d3b
Mohamed Gammal <73348912+mhammadgammal@users.noreply.github.com>
2022-12-14 13:15:14 +0200

    Update SaveReminderViewModelTest.kt

--- Location Reminder/starter/app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ Location Reminder/starter/app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,77 +1,16 @@
 package com.udacity.project4.locationreminders.savereminder
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.common.truth.Truth.assertThat
-import com.udacity.project4.R
-import com.udacity.project4.locationreminders.MainCoroutineRule
-import com.udacity.project4.locationreminders.data.FakeDataSource
-import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
-import getOrAwaitValue
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
-import org.koin.core.context.stopKoin
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
 
 
-    @get:Rule
-    var instantExecutorRule = InstantTaskExecutorRule()
+    //TODO: provide testing to the SaveReminderView and its live data objects
 
-    @get:Rule
-    var mainCoroutineRule = MainCoroutineRule()
-
-
-
-    private lateinit var remindersRepository: FakeDataSource
-
-    //Subject under test
-    private lateinit var viewModel: SaveReminderViewModel
-
-    @Before
-    fun setupViewModel() {
-        remindersRepository = FakeDataSource()
-        viewModel = SaveReminderViewModel(ApplicationProvider.getApplicationContext(), remindersRepository)
-    }
-
-    @After
-    fun tearDown() {
-        stopKoin()
-    }
-
-    @Test
-    fun validateEnteredData_EmptyTitleAndUpdateSnackBar() {
-        val reminder = ReminderDataItem("", "Description", "My School", 7.32323, 6.54343)
-
-        assertThat(viewModel.validateEnteredData(reminder)).isFalse()
-        assertThat(viewModel.showSnackBarInt.getOrAwaitValue()).isEqualTo(R.string.err_enter_title)
-    }
-
-    @Test
-    fun validateEnteredData_EmptyLocationAndUpdateSnackBar() {
-        val reminder = ReminderDataItem("Title", "Description", "", 7.32323, 6.54343)
-
-        assertThat(viewModel.validateEnteredData(reminder)).isFalse()
-        assertThat(viewModel.showSnackBarInt.getOrAwaitValue()).isEqualTo(R.string.err_select_location)
-    }
-
-
-    @Test
-    fun saveReminder_showLoading(){
-        val reminder = ReminderDataItem("Title", "Description", "Airport", 7.32323, 6.54343)
-        mainCoroutineRule.pauseDispatcher()
-        viewModel.saveReminder(reminder)
-        assertThat(viewModel.showLoading.getOrAwaitValue()).isTrue()
-        mainCoroutineRule.resumeDispatcher()
-        assertThat(viewModel.showLoading.getOrAwaitValue()).isFalse()
-    }
 
 }
